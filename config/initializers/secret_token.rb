a656aabd95eaa70973e0fdfa696f6d428ba644de
quest <quest@mac.com>
2011-07-18 23:32:05 -0700

    fresh rails app minus test unit support

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Googlepoint::Application.config.secret_token = 'f56366c014776c7181c7c8108bdf4dad9c747c90fd470ff38284e8c872c1054138cd4ed2d863b0932db98027c9c1a5bcdc483dc07c2f3e793400b5e5453dbef7'
