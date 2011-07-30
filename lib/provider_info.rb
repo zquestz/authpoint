346e5807461fa818d2e2aae837d2ac0acebe49ca
quest <quest@mac.com>
2011-07-30 00:49:45 -0700

    Added better error handling around loading providers.yml

--- lib/provider_info.rb
+++ lib/provider_info.rb
@@ -1,11 +1,18 @@
 class ProviderInfo
   extend ActiveSupport::Memoizable
 
+  def config_file
+    File.join(Rails.root, 'config', 'providers.yml')
+  end
+
   def settings
-    config = YAML.load(File.read(File.join(Rails.root, 'config', 'providers.yml')))
+    config = YAML.load(File.read(config_file))
     default_settings = config['default'] || {}
     env_settings = config[Rails.env] || {}
     default_settings.recursive_merge(env_settings)
+  rescue Errno::ENOENT => e
+    puts "#{e.message}" unless Rails.env.test?
+    exit
   end
   memoize :settings
 end
--- spec/models/provider_info_spec.rb
+++ spec/models/provider_info_spec.rb
@@ -1,10 +1,30 @@
 require 'spec_helper'
 
 describe ProviderInfo do
-  context "#settings" do
-    it "should return a hash of provider data" do
-      info = ProviderInfo.new.settings
-      info.should be_a(Hash)
+  describe '#settings' do
+    context "providers.yml is present" do
+      it "should return a hash of provider data" do
+        info = ProviderInfo.new.settings
+        info.should be_a(Hash)
+      end
+    end
+
+    context 'providers.yml is missing' do
+      before do
+        @provider_info = ProviderInfo.new
+        @provider_info.stub!(:config_file).and_return('')
+      end
+
+      it "should raise an error" do
+        lambda { @provider_info.settings }.should raise_error
+      end
+    end
+  end
+
+  context '#config_file' do
+    it "should return the path to provider.yml" do
+      p = ProviderInfo.new
+      p.config_file.should == File.join(Rails.root, 'config', 'providers.yml')
     end
   end
 end
