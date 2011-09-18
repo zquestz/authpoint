23742fcbb52bdbe22fd22c569efcb377debb6aba
quest <quest@mac.com>
2011-09-17 20:14:30 -0700

    Added global javascript for basic ui transitions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,10 +8,3 @@
 //= require jquery_ujs
 //= require jquery-ui
 //= require_tree .
-
-// Rollup the errors and notices after a few seconds.
-$(document).ready(function() {
-  if ($('.rollup')) {
-    $('.rollup').delay(3000).slideUp(500);
-  }
-});
---
+++ app/assets/javascripts/global.js.coffee
@@ -0,0 +1,2 @@
+$(document).ready ->
+  $(".rollup").delay(3000).slideUp(500) if $(".rollup")
