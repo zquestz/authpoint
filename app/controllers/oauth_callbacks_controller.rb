39133df829e2dc16014e7d122153d82c67a08d74
quest <quest@mac.com>
2011-09-17 20:26:51 -0700

    Made comment more accurate on oauth callback

--- app/controllers/oauth_callbacks_controller.rb
+++ app/controllers/oauth_callbacks_controller.rb
@@ -2,7 +2,7 @@ class OauthCallbacksController < ApplicationController
   skip_before_filter :require_login, :store_location
   before_filter :init_user, :except => [:failure, :logout]
 
-  # Auth with google
+  # Auth with google and other services.
   def google
     flash[:notice] = t(:auth_success, :scope => [:flash])
     redirect_back_or_default(root_path)
